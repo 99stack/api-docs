4e7dbdd48777b1239339564a0f5256b0870fe59d
The_Moose <alexander.albonius@protonmail.com>
2020-11-12 11:44:42 +0100

    misc: compiled beta

--- compile.bash
+++ compile.bash
@@ -2,4 +2,4 @@
 
 # Compile all the files into one for quicker loading into redoc
 # requires 'speccy' npm to be installed: 'npm install -g speccy@0.8.7'
-speccy resolve api.99stack.com.v-1.1.yaml -o docs-v-1.1.yaml
+speccy resolve api.99stack.com.v-1.1.yaml -o beta-docs-v-1.1.yaml
