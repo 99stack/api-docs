7d49ffa562923f3255d21390c295b373990e6470
The_Moose <alexander.albonius@protonmail.com>
2020-11-12 06:58:28 +0100

    compilation script

---
+++ compile.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Compile all the files into one for quicker loading into redoc
+# requires 'speccy' npm to be installed: 'npm install -g speccy@0.8.7'
+speccy resolve api.99stack.com.v-1.1.yaml -o docs-v-1.1.yaml
