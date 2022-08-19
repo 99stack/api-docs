845bf3c1a910e4dce885135a07a0160f59b9d0a3
BuildTools <unconfigured@null.spigotmc.org>
2022-08-19 14:19:47 +0200

    v-1.2

--- compile.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Compile all the files into one for quicker loading into redoc
-# requires 'speccy' npm to be installed: 'npm install -g speccy'
-speccy resolve api.99stack.com.v-1.1.yaml -o beta-docs-v-1.1.yaml
